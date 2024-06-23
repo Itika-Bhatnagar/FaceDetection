7a2b6d60f62f4bbdf25271fb8431a8d76b3cd6f1
Itika-Bhatnagar <bhatnagaritika@kgpian.iitkgp.ac.in>
2024-06-24 02:28:34 +0530

    string to detect faces is added

--- FaceDetection/test.cpp
+++ FaceDetection/test.cpp
@@ -67,6 +67,8 @@ int main() {
         // Draw rectangles around detected faces
         for (size_t i = 0; i < faces.size(); i++) {
             rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+            rectangle(img, Point(0, 0), Point(250, 70), Scalar(50, 50, 255), FILLED);
+            putText(img, to_string(faces.size()) + " Face Found", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
         }
 
         // Display the frame
