310c93a4c1e3fb3dafbe328363cc25641bddf204
Itika-Bhatnagar <bhatnagaritika@kgpian.iitkgp.ac.in>
2024-06-24 01:05:35 +0530

    camera access

--- FaceDetection/test.cpp
+++ FaceDetection/test.cpp
@@ -7,9 +7,23 @@ using namespace std;
 using namespace cv;
 
 
+/*
 void main() {
 	string path = "1718280956569.jpg";
 	Mat img = imread(path);
 	imshow("Frame", img);
 	waitKey(0);
+}
+*/
+
+void main()
+{
+	VideoCapture video(0);
+	Mat img;
+	while (true) {
+		video.read(img);
+		imshow("Frame", img);
+		waitKey(1);
+
+	}
 }
