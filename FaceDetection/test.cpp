3b5ef19d71c7e3697474ec40baf25068aa43fd75
Itika-Bhatnagar <bhatnagaritika@kgpian.iitkgp.ac.in>
2024-06-24 00:52:52 +0530

    added void main() function .

--- FaceDetection/test.cpp
+++ FaceDetection/test.cpp
@@ -1,2 +1,15 @@
 #include<opencv2/imgcodecs.hpp>
 #include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+
+
+void main() {
+	string path = "1718280956569.jpg";
+	Mat img = imread(path);
+	imshow("Frame", img);
+	waitKey(0);
+}
